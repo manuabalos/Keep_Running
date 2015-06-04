1329aeab801c8a32d3c7d0e9e087350e255e075d
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-06-04 20:54:08 +0200

    Añadidas nuevas rutas

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,8 @@
 	Waypoint.create( route_id: 1, latitude: 40.419230, longitude: -3.688471)
 	Waypoint.create( route_id: 1, latitude: 40.421059, longitude: -3.682410)
 
+	# ================================================================================================= 
+
 	Route.create( name: "Parque del Canal", description: "Recorrido por la zona exterior del parque del Canal.",
 				  location: "Madrid", difficulty: "Principiantes", latitude: 40.443223, longitude: -3.707657,
 				  origin_lat: 40.444143, origin_lng: -3.709544,
@@ -26,6 +28,8 @@
 	Waypoint.create( route_id: 2, latitude: 40.444988, longitude: -3.707009)
 	Waypoint.create( route_id: 2, latitude: 40.442142, longitude: -3.705201)
 	Waypoint.create( route_id: 2, latitude: 40.441227, longitude: -3.707658)
+
+	# ================================================================================================= 
 
 	Route.create( name: "Poligono Industrial", description: "Recorrido por la zona del poligono industrial y el barrio La Paz.",
 				  location: "Linares", difficulty: "Dificil", latitude: 38.088114, longitude: -3.639998,
@@ -38,4 +42,36 @@
 	Waypoint.create( route_id: 3, latitude: 38.085855, longitude: -3.640502)
 	Waypoint.create( route_id: 3, latitude: 38.082215, longitude: -3.616808)
 	Waypoint.create( route_id: 3, latitude: 38.088202, longitude: -3.625327)
+
+	# =================================================================================================
+
+	Route.create( name: "Zona Verde", description: "Recorrido por la zona del paseo de Linares y la Zona Verde.",
+				  location: "Linares", difficulty: "Intermedia", latitude: 38.098376, longitude: -3.632338,
+				  origin_lat: 38.098376, origin_lng: -3.632338,
+				  destination_lat: 38.098570, destination_lng: -3.632649)
+
+	Waypoint.create( route_id: 4, latitude: 38.130495, longitude: -3.611598)
+	Waypoint.create( route_id: 4, latitude: 38.126405, longitude: -3.616785)
+
+    # ================================================================================================= 
+
+	Route.create( name: "Templo de Debod", description: "Recorrido por la zona del paseo Pintor Rosales y el templo de Debod.",
+				  location: "Madrid", difficulty: "Principiantes", latitude: 40.432529, longitude: -3.725233,
+				  origin_lat: 40.432529, origin_lng: -3.725233,
+				  destination_lat: 40.432529, destination_lng: -3.725233)
+
+	Waypoint.create( route_id: 5, latitude: 40.422650, longitude: -3.716472)
+	Waypoint.create( route_id: 5, latitude: 40.424624, longitude: -3.719148)
+
+	# ================================================================================================= 
+
+	Route.create( name: "Paseo Marítimo", description: "Recorrido por el paseo marítimo.",
+				  location: "Barcelona", difficulty: "Principiantes", latitude: 41.380827, longitude: 2.183155,
+				  origin_lat: 41.380827, origin_lng: 2.183155,
+				  destination_lat: 41.406231, destination_lng: 2.219226)
+
+	#Waypoint.create( route_id: 6, latitude: 41.383061, longitude: 2.188505)
+	# =================================================================================================
+
+
 
