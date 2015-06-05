b197efe2532c4bbbc228b40d3d7c72697ba927ec
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-06-05 11:33:18 +0200

    Creados el control de sesiones

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/' => 'routes#index'
   get 'signup'  => 'users#new' 
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
   resources :routes do
