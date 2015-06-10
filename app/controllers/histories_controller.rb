033bb54ce941099294ee23814336fbc92b6c4c3a
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-06-10 15:46:37 +0200

    Trabajando en el estilo del historial

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -4,9 +4,6 @@ class HistoriesController < ApplicationController
 		@user = User.find current_user.id
 		@history = @user.routes
 		@history_date = History.where("user_id = ?", current_user.id)
-
-		
-	
 	end
 
 		private
--- db/schema.rb
+++ db/schema.rb
@@ -42,15 +42,6 @@ ActiveRecord::Schema.define(version: 20150610081407) do
     t.datetime "updated_at",      null: false
   end
 
-  create_table "routes_users", force: :cascade do |t|
-    t.integer "route_id"
-    t.integer "user_id"
-    t.date    "date"
-  end
-
-  add_index "routes_users", ["route_id"], name: "index_routes_users_on_route_id", using: :btree
-  add_index "routes_users", ["user_id"], name: "index_routes_users_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "uid"
     t.string   "name"
