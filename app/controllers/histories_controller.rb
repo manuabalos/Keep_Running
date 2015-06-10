f275ef13101a0318c82a0634e5b383aa5dda0d7b
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-06-10 17:14:33 +0200

    Añadido el historial en el perfil

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,8 +2,8 @@ class HistoriesController < ApplicationController
 	before_action :set_auth
 	def index
 		@user = User.find current_user.id
-		@history = @user.routes
-		@history_date = History.where("user_id = ?", current_user.id)
+		@history = @user.routes.order(created_at: :desc).limit(10)
+		@history_date = History.where("user_id = ?", current_user.id).order(created_at: :desc).limit(10)
 	end
 
 		private
