7542d0a378fd7a0ae1de424c952f1474c5cf27ff
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-06-17 19:02:44 +0200

    Trabajando en el sistema de ladder

--- app/controllers/ladders_controller.rb
+++ app/controllers/ladders_controller.rb
@@ -2,7 +2,8 @@ class LaddersController < ApplicationController
 	before_action :set_auth
 
 	def index
-
+		@toprunners = User.all.order(level: :desc).limit(3)
+		@runners = User.all.order(level: :desc)
 	end
 
 	private
