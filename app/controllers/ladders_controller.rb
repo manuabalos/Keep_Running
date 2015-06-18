95a7560d190c8ce83c1361138cb5734ce0f87e1f
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-06-18 10:14:30 +0200

    Fase beta 2.0 de sistema ladder

--- app/controllers/ladders_controller.rb
+++ app/controllers/ladders_controller.rb
@@ -2,8 +2,9 @@ class LaddersController < ApplicationController
 	before_action :set_auth
 
 	def index
-		@toprunners = User.all.order(level: :desc).limit(3)
-		@runners = User.all.order(level: :desc)
+		#@toprunners = User.all.order(level: :desc, experience: :desc).limit(3)
+		@toprunner = User.order(level: :desc, experience: :desc).first
+		@runners = User.all.order(level: :desc, experience: :desc)
 	end
 
 	private
